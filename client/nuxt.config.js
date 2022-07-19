8729aeefc65dd707025461c1f3be40deb9fddd29
Dmitry Kalalb <gdydhd0134h@gmail.com>
2022-07-19 11:02:17 +0200

    telegram fix

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -14,12 +14,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src: 'https://telegram.org/js/telegram-web-app.js'
+      }
     ]
-    // script: [
-    //   {
-    //     src: 'https://telegram.org/js/telegram-web-app.js'
-    //   }
-    // ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
